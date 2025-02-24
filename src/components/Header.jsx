c28d2dd534ee6085ed406b8dbfa67569d0427f2b
NehaTShigvan <121689166+NehaTShigvan@users.noreply.github.com>
2025-02-25 00:14:47 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import StickyNote2Icon from "@mui/icons-material/StickyNote2";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <StickyNote2Icon fontSize="medium" />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./components/App";
+// import "./../public/styles.css";
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+// import "./../public/styles.css";
+import "./styles.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
